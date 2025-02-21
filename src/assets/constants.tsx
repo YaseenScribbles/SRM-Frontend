b909cffe4de111a8498b1230d4dfd86d8d030422
YaseenScribbles <yaseen0696@gmail.com>
2025-02-21 12:38:47 +0530

    add url fix to access from local

--- src/assets/constants.tsx
+++ src/assets/constants.tsx
@@ -1,5 +1,25 @@
-export const url = "http://192.168.10.65:8000/api/";
-export const storageUrl = "http://192.168.10.65:8000/storage/";
+const isLocalNetwork = () => {
+  const localIPs = ["localhost", "127.0.0.1"]; // Add more if needed
+  const hostname = window.location.hostname;
+
+  // Check if the hostname is a local IP or part of a private network
+  if (
+    localIPs.includes(hostname) ||
+    hostname.startsWith("192.168.")
+  ) {
+    return true; // Inside local network
+  }
+  return false; // Outside (public network)
+};
+
+export const url = isLocalNetwork()
+  ? import.meta.env.VITE_API_URL_LOCAL
+  : import.meta.env.VITE_API_URL;
+
+export const storageUrl = isLocalNetwork()
+  ? import.meta.env.VITE_API_STORAGE_URL_LOCAL
+  : import.meta.env.VITE_API_STORAGE_URL;
+
 export const colorPrimary = "#264653";
 export const colorSecondary = "#2a9d8f";
 export const colorGreyLight1 = "#faf9f9";
@@ -9,11 +29,11 @@ export const colorGreyLight4 = "#ccc";
 export const colorGreyDark2 = "#777";
 export const colorGreyDark3 = "#999";
 export const menus = {
-    Dashboard: "Dashboard",
-    User: "User",
-    Purpose: "Purpose",
-    Contact: "Contact",
-    Distributor: "Distributor",
-    Visit: "Visit",
-    Order: "Order",
-}
+  Dashboard: "Dashboard",
+  User: "User",
+  Purpose: "Purpose",
+  Contact: "Contact",
+  Distributor: "Distributor",
+  Visit: "Visit",
+  Order: "Order",
+};
